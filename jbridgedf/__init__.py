1bab36477c2e53f6dbc7e74d980ea4e703603b61
ODenteAzul <odenteazul@gmail.com>
2025-06-27 21:34:35 -0300

    pyproject.toml and readme.md

--- jbridgedf/__init__.py
+++ jbridgedf/__init__.py
@@ -1,3 +1,3 @@
 from .parser import APIDataParser
 
-__version__ = "0.2.0"
+__version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jbridgedf",
-    version="0.1.0",
+    version="0.1.1",
     description="Bridge JSON APIs into clean, time-aware DataFrames.",
     author="Luis Felipe de Moraes",
     author_email="luis.felipe@email.com",
